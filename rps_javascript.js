19a0fc119a090263a5ef27a29a8fad0ad69104d3
Jerald Washington <jeraldwashington1@gmail.com>
2022-11-29 20:36:31 -0500

    Added round of paper scissors rock to create function with stored data

--- rps_javascript.js
+++ rps_javascript.js
@@ -5,3 +5,22 @@ function getComputerChoice() {
     let random = rps[Math.floor(Math.random() * rps.length)];
     return random;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return ("It's a draw!");
+    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        return ("You win! Rock beats scissors");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return ("You lose! Paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return ("You win! Paper beats rock");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return ("You lose! Scissors beat paper");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return ("You win! Scissors beat paper");
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return ("You lose! Rock beats scissors");
+    }
+}
+
